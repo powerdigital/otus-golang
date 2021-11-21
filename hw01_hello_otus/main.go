346b6a69ec6afb4b91f4e2a4a155ffb5399e7a48
Dilyaver Saliev <dilyaver.saliev@vseinstrumenti.ru>
2021-11-21 17:38:11 +0300

    fix lint error

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"golang.org/x/example/stringutil"
 )
 
 func main() {
-	var reverted = stringutil.Reverse("Hello, OTUS!")
+	reverted := stringutil.Reverse("Hello, OTUS!")
 	fmt.Println(reverted)
 }
