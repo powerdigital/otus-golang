8198538b1987921c5400c02eb3eef0f8c828eacd
Dilyaver Saliev <dilyaver.saliev@vseinstrumenti.ru>
2021-12-05 19:48:33 +0300

    HW3 is complete

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,51 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type WordCalc struct {
+	word  string
+	count int
+}
+
+func Top10(text string) []string {
+	if len(text) == 0 {
+		return []string{}
+	}
+
+	words := strings.Fields(text)
+	calculated := map[string]int{}
+
+	for _, word := range words {
+		calculated[word]++
+	}
+
+	wordSlice := []WordCalc{}
+
+	for word, count := range calculated {
+		wordSlice = append(wordSlice, WordCalc{word, count})
+	}
+
+	sort.Slice(wordSlice, func(i, j int) bool {
+		return wordSlice[i].count > wordSlice[j].count
+	})
+
+	topSlice := wordSlice[:10]
+	result := []string{}
+
+	sort.Slice(topSlice, func(i, j int) bool {
+		if wordSlice[i].count == wordSlice[j].count {
+			return wordSlice[i].word < wordSlice[j].word
+		}
+
+		return wordSlice[i].count > wordSlice[j].count
+	})
+
+	for _, wordCalc := range topSlice {
+		result = append(result, wordCalc.word)
+	}
+
+	return result
 }
